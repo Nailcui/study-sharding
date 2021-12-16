9ba56f044e5a3788940acab7e3afc7daf6c11aab
cui dingyu <cuidingyu@cuideMacBook-Pro.local>
2021-12-16 17:14:43 +0800

    add select api

--- spring-sharding/src/main/java/com/example/spring/sharding/controller/EnumsController.java
+++ spring-sharding/src/main/java/com/example/spring/sharding/controller/EnumsController.java
@@ -2,8 +2,11 @@ package com.example.spring.sharding.controller;
 
 import com.example.spring.sharding.dao.EnumsMapper;
 import com.example.spring.sharding.dataobject.EnumsDO;
+import com.example.spring.sharding.qo.EnumsQO;
 import java.util.Date;
+import java.util.List;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dingyu
  * @date 2021-12-15 19:10
  */
+@Slf4j
 @RestController
 @RequestMapping("/enums")
 public class EnumsController {
 
   @Resource
   private EnumsMapper enumsMapper;
+
+  @GetMapping("/0enums")
+  public List<EnumsDO> getAll0Enums() {
+    EnumsQO qo = new EnumsQO();
+    qo.setType(0);
+    return enumsMapper.selectAll(qo);
+  }
 
   @GetMapping("/add")
   public String add() {
